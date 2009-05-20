610336290dbfd7205f981d7a269f7f5d20b55270
Samppa Saarela <samppa.saarela@mysema.com>
2009-05-20 12:11:04 +0000

    javadocs

--- rdfbean-core/src/main/java/com/mysema/rdfbean/object/FlushMode.java
+++ rdfbean-core/src/main/java/com/mysema/rdfbean/object/FlushMode.java
@@ -4,11 +4,23 @@
 package com.mysema.rdfbean.object;
 
 /**
+ * FlushMode defines the strategy of how and when saved (see {@link Session#save(Object)})
+ * objects are actually persisted.
+ * 
  * @author sasa
- *
  */
 public enum FlushMode {
+    /**
+     * Changes are persisted after each {@link Session#save(Object)} or 
+     * {@link Session#saveAll(Object...)} call.
+     */
     ALWAYS, 
+    /**
+     * Changes are persisted only when {@link Session#flush()} is called.
+     */
     MANUAL,
+    /**
+     * Changes are persisted automatically just before committing a transaction. 
+     */
     COMMIT
 }
