8d174118480058a9e564dc195b4f907711eab89e
Timo Westkämper <timo.westkamper@mysema.com>
2010-05-09 17:25:29 +0000

    added test for MemoryIdSequence

---
+++ rdfbean-core/src/test/java/com/mysema/rdfbean/model/MemoryIdSequenceTest.java
@@ -0,0 +1,22 @@
+package com.mysema.rdfbean.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * MemoryIdSequenceTest provides
+ *
+ * @author tiwe
+ * @version $Id$
+ */
+public class MemoryIdSequenceTest {
+
+    @Test
+    public void testGetNextId() {
+        IdSequence idSequence = new MemoryIdSequence();
+        assertEquals(1l, idSequence.getNextId());
+        assertEquals(2l, idSequence.getNextId());
+    }
+
+}
