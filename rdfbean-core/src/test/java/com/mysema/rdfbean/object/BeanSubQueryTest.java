9b23bb6c8373f0650af834c99f6105698be8f233
Timo Westkämper <timo.westkamper@mysema.com>
2010-05-09 17:30:48 +0000

    added test for BeanSubQuery

---
+++ rdfbean-core/src/test/java/com/mysema/rdfbean/object/BeanSubQueryTest.java
@@ -0,0 +1,65 @@
+package com.mysema.rdfbean.object;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mysema.query.types.path.PString;
+import com.mysema.query.types.path.PathBuilder;
+
+/**
+ * BeanSubQueryTest provides
+ *
+ * @author tiwe
+ * @version $Id$
+ */
+public class BeanSubQueryTest {
+    
+    private BeanSubQuery beanSubQuery = new BeanSubQuery();
+
+    private PathBuilder<Object> pathBuilder = new PathBuilder<Object>(Object.class, "obj");
+    
+    private PString stringPath = pathBuilder.getString("str");
+    
+    @Before
+    public void setUp(){
+        beanSubQuery.from(pathBuilder);
+    }
+    
+    @Test
+    public void testCount() {
+        beanSubQuery.count();
+    }
+
+    @Test
+    public void testExists() {
+        beanSubQuery.exists();
+    }
+
+    @Test
+    public void testListExprOfQExprOfQExprOfQArray() {
+        beanSubQuery.list(pathBuilder);
+    }
+
+    @Test
+    public void testNotExists() {
+        beanSubQuery.notExists();
+    }
+
+    @Test
+    public void testOrderBy() {
+        beanSubQuery.orderBy(stringPath.asc());
+    }
+
+    @Test
+    public void testUniqueEBoolean() {
+        beanSubQuery.unique(stringPath);
+    }
+
+    @Test
+    public void testWhere() {
+        beanSubQuery.where(stringPath.isNotNull());
+    }
+
+}
