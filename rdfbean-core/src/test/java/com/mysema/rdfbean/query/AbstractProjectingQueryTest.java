66c5b2e6c5dba5660afbdeabc938d002aa36fa2b
Timo Westkämper <timo.westkamper@mysema.com>
2010-05-25 18:54:38 +0000

    added tests

---
+++ rdfbean-core/src/test/java/com/mysema/rdfbean/query/AbstractProjectingQueryTest.java
@@ -0,0 +1,42 @@
+package com.mysema.rdfbean.query;
+
+import org.junit.Test;
+
+import com.mysema.query.types.path.PathBuilder;
+import com.mysema.rdfbean.TEST;
+import com.mysema.rdfbean.annotations.ClassMapping;
+import com.mysema.rdfbean.annotations.Id;
+import com.mysema.rdfbean.model.IDType;
+import com.mysema.rdfbean.model.MiniDialect;
+import com.mysema.rdfbean.object.Session;
+import com.mysema.rdfbean.object.SessionUtil;
+
+
+public class AbstractProjectingQueryTest {
+    
+    @ClassMapping(ns=TEST.NS)
+    public static class Entity{
+	
+	@Id(IDType.LOCAL)
+	String id;
+    }
+    
+    @Test
+    public void test(){
+	Session session = SessionUtil.openSession(Entity.class);
+	MiniDialect dialect = new MiniDialect();
+	PathBuilder<Object> entity = new PathBuilder<Object>(Object.class,"obj");
+	DummyQuery query = new DummyQuery(dialect,session).from(entity);
+	query.count();
+	query.countDistinct();
+	query.iterate(entity);
+	query.iterateDistinct(entity);
+	query.list(entity);
+	query.listDistinct(entity);
+	query.map(entity, entity);
+	query.uniqueResult(entity);
+	
+	query.listResults(entity);
+    }
+
+}
