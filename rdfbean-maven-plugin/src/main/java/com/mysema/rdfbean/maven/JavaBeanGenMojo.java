ec6161f513df1b508d3ceeb4762b7faae15c9b01
Timo Westkämper <timo.westkamper@mysema.com>
2010-07-31 16:48:53 +0000

    updated version to 1.3.2

---
+++ rdfbean-maven-plugin/src/main/java/com/mysema/rdfbean/maven/JavaBeanGenMojo.java
@@ -0,0 +1,23 @@
+package com.mysema.rdfbean.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * JavaBeanGenMojo provides a Maven plugin for Java Bean generation from OWL/RDF-S schemas
+ *
+ * @author tiwe
+ * @version $Id$
+ * 
+ * @goal beangen
+ * @phase generate-sources
+ */
+public class JavaBeanGenMojo extends AbstractMojo{
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        // TODO
+    }
+
+}
