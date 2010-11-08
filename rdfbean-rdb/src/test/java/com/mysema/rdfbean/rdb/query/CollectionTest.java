8fe05616bda92d2b4c0c9aa16f42e8df1005ac57
Timo Westkämper <timo.westkamper@mysema.com>
2010-11-08 09:23:15 +0000

    refactored tests

--- rdfbean-rdb/src/test/java/com/mysema/rdfbean/rdb/query/CollectionTest.java
+++ rdfbean-rdb/src/test/java/com/mysema/rdfbean/rdb/query/CollectionTest.java
@@ -5,6 +5,7 @@
  */
 package com.mysema.rdfbean.rdb.query;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.mysema.query.alias.Alias;
@@ -22,21 +23,30 @@ import com.mysema.rdfbean.testutil.SessionConfig;
 @SessionConfig({Company.class, Department.class, Employee.class})
 public class CollectionTest extends AbstractRDBTest implements CompanyDepartmentEmployeeDomain{
     
-    @Test
-    public void test(){        
+    private Employee emp1, emp2, emp3, emp4;
+    
+    @Before
+    public void setUp(){
         Department dep1 = new Department();
         session.saveAll(dep1, new Department());
         
-        Employee emp1 = new Employee();
+        emp1 = new Employee();
         emp1.department = dep1;
-        Employee emp2 = new Employee();
-        Employee emp3 = new Employee();
-        Employee emp4 = new Employee();
-        session.saveAll(emp1, emp2, emp3, emp4);
-        
+        emp2 = new Employee();
+        emp3 = new Employee();
+        emp4 = new Employee();
+        session.saveAll(emp1, emp2, emp3, emp4);         
+    }
+    
+    @Test
+    public void Contains(){               
         Department dep = Alias.alias(Department.class);
-        Employee emp = Alias.alias(Employee.class);
         assertEquals(1l, session.from($(dep)).where($(dep.getEmployees()).contains(emp1)).count());
+    }
+    
+    @Test
+    public void In(){
+        Employee emp = Alias.alias(Employee.class);
         assertEquals(3l, session.from($(emp)).where($(emp).in(emp1, emp2, emp3)).count());
     }
 
--- rdfbean-sesame/src/test/java/com/mysema/rdfbean/sesame/query/CollectionTest.java
+++ rdfbean-sesame/src/test/java/com/mysema/rdfbean/sesame/query/CollectionTest.java
@@ -3,6 +3,7 @@ package com.mysema.rdfbean.sesame.query;
 import static com.mysema.query.alias.Alias.$;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.mysema.query.alias.Alias;
@@ -16,22 +17,31 @@ import com.mysema.rdfbean.testutil.SessionConfig;
 @SessionConfig({Company.class, Department.class, Employee.class})
 public class CollectionTest extends SessionTestBase implements CompanyDepartmentEmployeeDomain{
     
-    @Test
-    public void test(){        
+    private Employee emp1, emp2, emp3, emp4;
+    
+    @Before
+    public void setUp(){
         Department dep1 = new Department();
         session.saveAll(dep1, new Department());
         
-        Employee emp1 = new Employee();
+        emp1 = new Employee();
         emp1.department = dep1;
-        Employee emp2 = new Employee();
-        Employee emp3 = new Employee();
-        Employee emp4 = new Employee();
-        session.saveAll(emp1, emp2, emp3, emp4);
-        
+        emp2 = new Employee();
+        emp3 = new Employee();
+        emp4 = new Employee();
+        session.saveAll(emp1, emp2, emp3, emp4);         
+    }
+    
+    @Test
+    public void Contains(){               
         Department dep = Alias.alias(Department.class);
-        Employee emp = Alias.alias(Employee.class);
         assertEquals(1l, session.from($(dep)).where($(dep.getEmployees()).contains(emp1)).count());
+    }
+    
+    @Test
+    public void In(){
+        Employee emp = Alias.alias(Employee.class);
         assertEquals(3l, session.from($(emp)).where($(emp).in(emp1, emp2, emp3)).count());
     }
-
+    
 }
