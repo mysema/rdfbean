c33ae97400d71c3df64021b64cb8581e549d1235
Timo Westkämper <timo.westkamper@mysema.com>
2010-11-08 13:19:23 +0000

    made SPARQLServlet extend HttpServlet

--- rdfbean-sparql/src/main/java/com/mysema/rdfbean/sparql/SPARQLServlet.java
+++ rdfbean-sparql/src/main/java/com/mysema/rdfbean/sparql/SPARQLServlet.java
@@ -2,11 +2,11 @@ package com.mysema.rdfbean.sparql;
 
 import java.io.IOException;
 
-import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -24,8 +24,10 @@ import com.mysema.rdfbean.model.io.Format;
  * @author tiwe
  *
  */
-public class SPARQLServlet implements Servlet{
+public class SPARQLServlet extends HttpServlet{
     
+    private static final long serialVersionUID = 5726683938555535282L;
+
     public static final String SPARQL_RESULTS_JSON = "application/sparql-results+json";
     
     public static final String SPARQL_RESULTS_XML = "application/sparql-results+xml";
