d2b5db45f361ae5c79602be0c1a29e65a4745c19
Timo Westkämper <timo.westkamper@mysema.com>
2010-11-17 12:06:40 +0000

    added tests for exists

--- rdfbean-sesame/src/test/java/com/mysema/rdfbean/sesame/SesameConnectionTest.java
+++ rdfbean-sesame/src/test/java/com/mysema/rdfbean/sesame/SesameConnectionTest.java
@@ -1,14 +1,22 @@
 package com.mysema.rdfbean.sesame;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Collections;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.mysema.rdfbean.TEST;
+import com.mysema.rdfbean.model.BID;
+import com.mysema.rdfbean.model.RDF;
 import com.mysema.rdfbean.model.RDFConnection;
+import com.mysema.rdfbean.model.RDFS;
 import com.mysema.rdfbean.model.Repository;
 import com.mysema.rdfbean.model.STMT;
+import com.mysema.rdfbean.model.UID;
 
 
 public class SesameConnectionTest {
@@ -35,6 +43,14 @@ public class SesameConnectionTest {
         conn.update(Collections.<STMT>emptySet(), null);
         conn.update(null, Collections.<STMT>emptySet());
         conn.update(null, null);
+    }
+
+    @Test
+    public void Exists(){
+        UID context = new UID(TEST.NS);
+        assertFalse(conn.exists(null, null, null, context, false));
+        conn.update(null, Collections.singleton(new STMT(new BID(), RDF.type, RDFS.Class, context)));
+        assertTrue(conn.exists(null, null, null, context, false));
     }
 
 }
