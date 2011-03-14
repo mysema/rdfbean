6f8c84f285a4e67418742210d0533c259d3ad702
Timo Westkämper <timo.westkamper@mysema.com>
2011-03-14 08:48:45 +0000

    splitted code into more methods

--- rdfbean-core/src/main/java/com/mysema/rdfbean/schema/SchemaGen.java
+++ rdfbean-core/src/main/java/com/mysema/rdfbean/schema/SchemaGen.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2010 Mysema Ltd.
  * All rights reserved.
- * 
+ *
  */
 package com.mysema.rdfbean.schema;
 
@@ -46,23 +46,22 @@ import com.mysema.rdfbean.xsd.ConverterRegistryImpl;
  * @author sasa
  *
  */
-// TODO : simplify
 public class SchemaGen {
-    
+
     private Configuration configuration;
-    
+
     private Repository repository;
-    
-    private ConverterRegistry converterRegistry = new ConverterRegistryImpl();
-    
-    private Set<String> exportNamespaces = new HashSet<String>();
-    
+
+    private final ConverterRegistry converterRegistry = new ConverterRegistryImpl();
+
+    private final Set<String> exportNamespaces = new HashSet<String>();
+
     private boolean useTypedLists = true;
-    
+
     private String ontology;
-    
+
     private String[] ontologyImports;
-    
+
     public void exportConfiguration() {
         SessionFactoryImpl sessionFactory = new SessionFactoryImpl();
         sessionFactory.setConfiguration(new DefaultConfiguration(RDFSClass.class.getPackage(), OWLClass.class.getPackage()));
@@ -86,19 +85,19 @@ public class SchemaGen {
         resources.put(RDFS.label, new RDFProperty(RDFS.label));
         resources.put(RDFS.comment, new RDFProperty(RDFS.comment));
         resources.put(RDFS.Resource, new RDFSClass<Object>(RDFS.Resource));
+
+        // process classes
         for (MappedClass mappedClass : configuration.getMappedClasses()) {
-            processClass(mappedClass.getJavaClass(), session, resources);
+            processClass(session, mappedClass.getJavaClass(), resources);
         }
+
+        // save resources
         session.saveAll(resources.values().toArray());
     }
 
     @SuppressWarnings("unchecked")
     @Nullable
-    private RDFSClass<RDFSResource> processClass(Class<?> clazz, Session session, Map<UID, RDFSResource> resources) {
-        // TODO : simplify
-//        if (clazz == null) {
-//            return null;
-//        }
+    private RDFSClass<RDFSResource> processClass(Session session, Class<?> clazz, Map<UID, RDFSResource> resources) {
         MappedClass mappedClass = configuration.getMappedClass(clazz);
         OWLClass owlClass = null;
         UID cuid = mappedClass.getUID();
@@ -107,7 +106,7 @@ public class SchemaGen {
                 owlClass = new ReferenceClass(cuid);
             } else {
                 owlClass = (OWLClass) resources.get(cuid);
-                
+
                 if (owlClass == null) {
                     owlClass = new OWLClass(cuid);
                     resources.put(cuid, owlClass);
@@ -116,17 +115,18 @@ public class SchemaGen {
                 } else {
                     return owlClass;
                 }
-    
+
                 // super class
                 if (clazz.getSuperclass() != null && !clazz.getSuperclass().equals(Object.class)){
-                    addParent(clazz.getSuperclass(), owlClass, session, resources);    
+                    addParent(clazz.getSuperclass(), owlClass, session, resources);
                 }
-                
+
                 // interfaces
                 for (Class<?> iface : clazz.getInterfaces()) {
                     addParent(iface, owlClass, session, resources);
                 }
-                
+
+                // enum instances
                 if (mappedClass.isEnum()) {
                     List<RDFSResource> oneOf = new ArrayList<RDFSResource>();
                     for (Object constant: clazz.getEnumConstants()) {
@@ -139,113 +139,121 @@ public class SchemaGen {
                 // properties
                 for (MappedPath mappedPath : mappedClass.getProperties()) {
                     if (!mappedPath.isInherited() && mappedPath.isSimpleProperty()) {
-                        MappedProperty<?> mappedProperty = mappedPath.getMappedProperty();
-                        MappedPredicate mappedPredicate = mappedPath.get(0);
-                        UID puid = mappedPredicate.getUID();
-                        String predicateNs = puid.ns();
-                        // No restrictions on predicates from built-in namespaces
-                        if (RDF.NS.equals(predicateNs) || RDFS.NS.equals(predicateNs) || OWL.NS.equals(predicateNs)) {
-                            continue;
-                        }
-                        final RDFProperty property;
-                        boolean seenProperty = resources.containsKey(puid);
-                        if (seenProperty) {
-                            property = (RDFProperty) resources.get(puid);
-                            if (mappedPath.isReference()) {
-                                if (!(property instanceof ObjectProperty)) {
-                                    throw new IllegalArgumentException("Expected ObjectProperty for: " + mappedPath);
-                                }
-                            } else  if (!mappedProperty.isAnyResource()){
-                                if (!(property instanceof DatatypeProperty)) {
-                                    throw new IllegalArgumentException("Expected DatatypeProperty for: " + mappedPath);
-                                }
-                            }
-                        } else {
-                            if (mappedProperty.isAnyResource()) {
-                                property = new RDFProperty(puid);
-                            } else if (mappedPath.isReference() || mappedProperty.isCollection()) {
-                                property = new ObjectProperty(puid);
-                            } else {
-                                property = new DatatypeProperty(puid);
-                            }
-                            property.setLabel(Locale.ROOT, puid.getLocalName());                            
-                            resources.put(puid, property);
-                            // label
-                        }
-                        
-                        final Restriction restriction = new Restriction();
-                        restriction.setOnProperty(property);
-                        
-                        // TODO range mismatches!
-                        if (mappedProperty.isCollection()) {
-                            if (mappedProperty.isList()) {
-                                if (property.getRange().isEmpty()) {
-                                    RDFSClass<?> componentType = 
-                                        processClass(mappedProperty.getComponentType(), session, resources);
-                                    if (useTypedLists && componentType != null) {
-                                        property.addRange(new TypedList(cuid.ns(), componentType));
-                                        // Protege doesn't support typed lists using allValuesFrom
-                                        //owlClass.setAllValuesFrom(property, new TypedList(cuid.ns(), componentType));
-                                    } else {
-                                        owlClass.setAllValuesFrom(property, (RDFSClass<RDFSResource>) resources.get(RDF.List));
-                                    }
-                                    restriction.setMaxCardinality(1);
-                                }
-                            } else {
-                                RDFSClass<?> componentType = 
-                                    processClass(mappedProperty.getComponentType(), session, 
-                                            resources);
-                                if (componentType != null) {
-                                    restriction.setAllValuesFrom(componentType);
-                                }
-                            }
-                        } else if (mappedPath.isReference()) {
-                            if (property.getRange().isEmpty()) {
-                                RDFSClass<?> range = processClass(mappedProperty.getType(), 
-                                        session, resources);
-                                if (range != null) {
-                                    owlClass.setAllValuesFrom(property, range);
-                                } else if (mappedProperty.isAnyResource()) {
-                                    owlClass.setAllValuesFrom(property, (RDFSClass<?>) resources.get(RDFS.Resource));
-                                }
-                            }
-                            restriction.setMaxCardinality(1);
-                        } else {
-                            if (property.getRange().isEmpty()) {
-                                UID range;
-                                if (mappedProperty.isAnyResource()) {
-                                    range = RDFS.Resource;
-                                }else if (mappedProperty.isLocalized()){    
-                                    range = RDF.text;
-                                } else {
-                                    range = converterRegistry.getDatatype(mappedProperty.getType());
-                                }
-                                if (range != null) {
-                                    owlClass.setAllValuesFrom(property, getDatatype(range, resources));
-                                }
-                            }
-                            restriction.setMaxCardinality(1);
-                        }
-                        if (restriction.isDefined()) {
-                            owlClass.addSuperClass(restriction);
-                        }
-                        
-                        if (mappedProperty.isRequired()) {
-                            final Restriction minCardinality = new Restriction();
-                            minCardinality.setOnProperty(property);
-                            minCardinality.setMinCardinality(1);
-                            owlClass.addSuperClass(minCardinality);
-                        }
-//                        if (!seenProperty) {
-//                            session.save(property);
-//                        }
+                        processProperty(session, resources, owlClass, cuid, mappedPath);
                     }
                 }
             }
         }
         return owlClass;
     }
-    
+
+    @SuppressWarnings("unchecked")
+    private void processProperty(Session session, Map<UID, RDFSResource> resources, OWLClass owlClass, UID cuid, MappedPath mappedPath) {
+        MappedProperty<?> mappedProperty = mappedPath.getMappedProperty();
+        MappedPredicate mappedPredicate = mappedPath.get(0);
+        UID puid = mappedPredicate.getUID();
+        String predicateNs = puid.ns();
+        // No restrictions on predicates from built-in namespaces
+        if (RDF.NS.equals(predicateNs) || RDFS.NS.equals(predicateNs) || OWL.NS.equals(predicateNs)) {
+           return;
+        }
+        final RDFProperty property;
+        boolean seenProperty = resources.containsKey(puid);
+
+        if (seenProperty) {
+            property = (RDFProperty) resources.get(puid);
+            if (mappedPath.isReference()) {
+                if (!(property instanceof ObjectProperty)) {
+                    throw new IllegalArgumentException("Expected ObjectProperty for: " + mappedPath);
+                }
+            } else  if (!mappedProperty.isAnyResource()){
+                if (!(property instanceof DatatypeProperty)) {
+                    throw new IllegalArgumentException("Expected DatatypeProperty for: " + mappedPath);
+                }
+            }
+
+        } else {
+            if (mappedProperty.isAnyResource()) {
+                property = new RDFProperty(puid);
+            } else if (mappedPath.isReference() || mappedProperty.isCollection()) {
+                property = new ObjectProperty(puid);
+            } else {
+                property = new DatatypeProperty(puid);
+            }
+            property.setLabel(Locale.ROOT, puid.getLocalName());
+            resources.put(puid, property);
+            // label
+        }
+
+        final Restriction restriction = new Restriction();
+        restriction.setOnProperty(property);
+
+        // TODO range mismatches!
+        if (mappedProperty.isCollection()) {
+            if (mappedProperty.isList()) {
+                if (property.getRange().isEmpty()) {
+                    RDFSClass<?> componentType =
+                        processClass(session, mappedProperty.getComponentType(), resources);
+                    if (useTypedLists && componentType != null) {
+                        property.addRange(new TypedList(cuid.ns(), componentType));
+                        // Protege doesn't support typed lists using allValuesFrom
+                        //owlClass.setAllValuesFrom(property, new TypedList(cuid.ns(), componentType));
+                    } else {
+                        owlClass.setAllValuesFrom(property, (RDFSClass<RDFSResource>) resources.get(RDF.List));
+                    }
+                    restriction.setMaxCardinality(1);
+                }
+            } else {
+                RDFSClass<?> componentType =
+                    processClass(session, mappedProperty.getComponentType(),
+                            resources);
+                if (componentType != null) {
+                    restriction.setAllValuesFrom(componentType);
+                }
+            }
+
+        // reference
+        } else if (mappedPath.isReference()) {
+            if (property.getRange().isEmpty()) {
+                RDFSClass<?> range = processClass(session,
+                        mappedProperty.getType(), resources);
+                if (range != null) {
+                    owlClass.setAllValuesFrom(property, range);
+                } else if (mappedProperty.isAnyResource()) {
+                    owlClass.setAllValuesFrom(property, (RDFSClass<?>) resources.get(RDFS.Resource));
+                }
+            }
+            restriction.setMaxCardinality(1);
+
+        // other
+        } else {
+            if (property.getRange().isEmpty()) {
+                UID range;
+                if (mappedProperty.isAnyResource()) {
+                    range = RDFS.Resource;
+                }else if (mappedProperty.isLocalized()){
+                    range = RDF.text;
+                } else {
+                    range = converterRegistry.getDatatype(mappedProperty.getType());
+                }
+                if (range != null) {
+                    owlClass.setAllValuesFrom(property, getDatatype(range, resources));
+                }
+            }
+            restriction.setMaxCardinality(1);
+        }
+        if (restriction.isDefined()) {
+            owlClass.addSuperClass(restriction);
+        }
+
+        if (mappedProperty.isRequired()) {
+            final Restriction minCardinality = new Restriction();
+            minCardinality.setOnProperty(property);
+            minCardinality.setMinCardinality(1);
+            owlClass.addSuperClass(minCardinality);
+        }
+    }
+
     private RDFSDatatype getDatatype(UID uid, Map<UID, RDFSResource> resources) {
         if (resources.containsKey(uid)) {
             return (RDFSDatatype) resources.get(uid);
@@ -256,24 +264,23 @@ public class SchemaGen {
         }
     }
 
-    private void addParent(Class<?> clazz, OWLClass owlClass, Session session, 
-            Map<UID, RDFSResource> resources) {
-        RDFSClass<RDFSResource> parent = processClass(clazz, session, resources);
+    private void addParent(Class<?> clazz, OWLClass owlClass, Session session, Map<UID, RDFSResource> resources) {
+        RDFSClass<RDFSResource> parent = processClass(session, clazz, resources);
         if (parent != null) {
             owlClass.addSuperClass(parent);
         }
     }
-    
+
     public SchemaGen addExportNamespaces(Set<String> namespaces) {
         exportNamespaces.addAll(namespaces);
         return this;
     }
-    
+
     public SchemaGen addExportNamespace(String ns) {
         exportNamespaces.add(ns);
         return this;
     }
-    
+
     public SchemaGen setConfiguration(Configuration configuration) {
         this.configuration = configuration;
         return this;
